196f0357ee11a3cc05aae9545caec4fafd1bcb3e
douglas <douglasmartinssantos7@gmail.com>
2026-04-20 18:45:59 -0300

    refactor: update cart clearing logic in cypress commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,12 +1,25 @@
 Cypress.Commands.add('emptyCartUi', () => {
-  cy.visit('/')
-  cy.contains('CARRINHO').click()
+  cy.visit('/cart.html')
+  
+  cy.get('#cart-list').should(($list) => {
+    expect($list.text()).to.match(/Seu carrinho está vazio|Remover/)
+  })
 
   const removeNext = () => {
     cy.get('body').then(($body) => {
-      if ($body.find('.remove-from-cart').length > 0) {
+      const $btns = $body.find('.remove-from-cart')
+      if ($btns.length > 0) {
+        const countBefore = $btns.length
+        cy.intercept('DELETE', '**/api/carrinho/**').as('deleteItem')
         cy.get('.remove-from-cart').first().click()
-        cy.wait(500)
+        cy.wait('@deleteItem')
+        
+        if (countBefore === 1) {
+          cy.contains('Seu carrinho está vazio.').should('be.visible')
+        } else {
+          cy.get('.remove-from-cart').should('have.length', countBefore - 1)
+        }
+        
         removeNext()
       }
     })
