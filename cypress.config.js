1add66a1871af0d3b6e6c96cc95493256d250393
douglas <douglasmartinssantos7@gmail.com>
2026-04-23 11:22:22 -0300

    new config in cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,26 +1,34 @@
-const { defineConfig } = require("cypress")
-const createBundler = require("@bahmutov/cypress-esbuild-preprocessor")
-const preprocessor = require("@badeball/cypress-cucumber-preprocessor")
-const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild")
-
-const { allureCypress } = require("allure-cypress/reporter")
-
-const cypressOnFix = require("cypress-on-fix")
+const { defineConfig } = require('cypress')
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor')
+const preprocessor = require('@badeball/cypress-cucumber-preprocessor')
+const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild')
+const { allureCypress } = require('allure-cypress/reporter')
+const cypressOnFix = require('cypress-on-fix')
+const db = require('./cypress/support/db')
 
 async function setupNodeEvents(on, config) {
   on = cypressOnFix(on)
-  
+
   await preprocessor.addCucumberPreprocessorPlugin(on, config)
 
   on(
-    "file:preprocessor",
+    'file:preprocessor',
     createBundler({
-      plugins: [createEsbuildPlugin.default(config)],
+      plugins: [createEsbuildPlugin.default(config)]
     })
   )
 
   allureCypress(on, config, {
-    resultsDir: "allure-results"
+    resultsDir: 'allure-results'
+  })
+
+  on('task', {
+    seedAdmin() {
+      return db.seedAdmin()
+    },
+    getUsers() {
+      return db.getUsers()
+    }
   })
 
   return config
@@ -29,8 +37,11 @@ async function setupNodeEvents(on, config) {
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents,
-    specPattern: ["**/*.feature", "**/*.cy.js"],
-    baseUrl: "http://127.0.0.1:3000",
-    experimentalWebKitSupport: true
-  },
+    specPattern: ['**/*.feature', '**/*.cy.js'],
+    baseUrl: 'http://127.0.0.1:3000',
+    experimentalWebKitSupport: true,
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    defaultCommandTimeout: 10000
+  }
 })
